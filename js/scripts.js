eb57e158e1569714be07712240c56349654f92e0
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-27 11:53:08 -0700

    add var fun and color to js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
  $(document).ready(function() {
-   $("form#question3").submit(function(event) {
+   $("form#question1").submit(function(event) {
+     var fun = $("select#fun").val();
+     var color = $("select#color").val();
      var iceCream = $("select#iceCream").val();
      var animal = $("select#animal").val();
      var coffee = $("select#coffee").val();
 
-     if (coffee === 'americano' && animal === 'cat' && iceCream === 'chocolate') {
+     if (coffee === 'americano' && animal === 'cat' && iceCream === 'chocolate' && color === 'green' && fun === 'book') {
        $('#americano').show();
-     } else if (coffee === 'mocha') {
+     } else if (coffee === 'mocha' && animal === 'dog' && iceCream === 'vanilla') {
         $('#mocha').show();
      } else if (coffee === 'frapuccino') {
        $('#frapuccino').show();
