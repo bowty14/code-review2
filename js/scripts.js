946d31fbdaa5628bedb44b890152e21f67d604b7
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-27 12:01:33 -0700

    add different combo that outputs #americano.

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,13 @@
 
      if (coffee === 'americano' && animal === 'cat' && iceCream === 'chocolate' && color === 'green' && fun === 'book') {
        $('#americano').show();
-     } else if (coffee === 'mocha' && animal === 'dog' && iceCream === 'vanilla') {
+     } else if (coffee === 'americano' && animal === 'dog' && iceCream === 'vanilla' && color === 'red' && fun === 'movie') {
+       $('#americano').show();
+     } else if (coffee === 'mocha' && animal === 'dog' && iceCream === 'vanilla' && color === 'red' && fun === 'movie') {
         $('#mocha').show();
-     } else if (coffee === 'frapuccino') {
+     } else if (coffee === 'frapuccino' && animal === 'bird' && iceCream === 'strawberry' && color === 'blue' && fun === 'hike') {
        $('#frapuccino').show();
-     }
+     } 
      event.preventDefault();
    })
   });
